868b1203244a7f7a979c03506821539981bd2b02
tofanwbakti <tofanwbakti@gmail.com>
2025-10-09 10:25:18 +0700

    Modifikasi [upload CV]

--- forms/contact.php
+++ forms/contact.php
@@ -6,10 +6,10 @@
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Replace contact@example.com with your real receiving email address
+$receiving_email_address = 'tofanwbakti@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+if (file_exists($php_email_form = 'https://tofanwbakti.github.io/assets/vendor/php-email-form/php-email-form.php')) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
